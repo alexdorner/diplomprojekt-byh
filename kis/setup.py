f3557af23aecfccef89d6862b5c1b964892453b7
Alexandra Dorner <56341637+alexdorner@users.noreply.github.com>
2021-02-01 20:17:54 +0100

    UPDATE

--- kis/setup.py
+++ kis/setup.py
@@ -1,17 +1,18 @@
 # -*- coding: utf-8 -*-
 from __future__ import unicode_literals
 from setuptools import setup, find_packages
-import re, ast
+import re, ast, os
 
 # get version from __version__ variable in erpnext/__init__.py
 _version_re = re.compile(r'__version__\s+=\s+(.*)')
+#
+#with open('requirements.txt') as f:
+#	install_requires = f.read().strip().split('\n')
 
-with open('requirements.txt') as f:
-	install_requires = f.read().strip().split('\n')
-
-with open('KIS/__init__.py', 'rb') as f:
-	version = str(ast.literal_eval(_version_re.search(
-		f.read().decode('utf-8')).group(1)))
+#with open('KIS/__init__.py', 'rb') as f:
+#	version = str(ast.literal_eval(_version_re.search(
+#		f.read().decode('utf-8')).group(1)))
+version = '0.0.1'
 
 setup(
 	name='KIS',
@@ -22,5 +23,5 @@ setup(
 	packages=find_packages(),
 	zip_safe=False,
 	include_package_data=True,
-	install_requires=install_requires
+	install_requires=("frappe",),
 )
