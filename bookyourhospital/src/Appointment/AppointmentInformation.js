6b54f9d802a8d65585f6141eadaaf2874725be23
jir17518 <jir17518@spengergasse.at>
2021-02-17 20:46:16 +0100

    update Listview Appointment

--- bookyourhospital/src/Appointment/AppointmentInformation.js
+++ bookyourhospital/src/Appointment/AppointmentInformation.js
@@ -1,42 +1,13 @@
+//Nach dem TAN
+//Termincode
+//Wird per SMS gesendet --> Beenden Button (Kommt man auf Home)
 import React, { Component } from 'react';
-import {Col, Container} from "react-bootstrap";
+import {Button, Table, Container, Row, Col, Dropdown, ListGroup, ListGroupItem} from 'react-bootstrap';
+import history from "../history";
+import ServiceUnit from "../Selection/ServiceUnit";
+import AppointmentOverview from "./AppointmentOverview";
 
-class AppointmentInformation extends Component {
-    appointmentView = this.props.match.params.appointmentView
+class AppointmentInformation extends Component{
 
-    async componentDidMount(){
-        try {
-            let result = await fetch("http://localhost:8080/api", {
-                method: 'post',
-                mode: "cors",
-                headers: {
-                    'Accept': 'application/json',
-                    'Content-type': 'application/json',
-                },
-                body: JSON.stringify({
-                    appointmentView: this.appointmentView
-                })
-            })
-        }
-        catch (e){
-            console.log(e);
-        }
-
-    }
-
-    render() {
-        return (
-            <Container>
-                <h1>Ausgewählter Termin: {this.appointmentView}</h1>
-                <Col>
-                    <p>Adresse so und so</p>
-                </Col>
-                <Col>
-                    <div style={{ height: '100vh', width: '100%' }}>
-                    </div>
-                </Col>
-            </Container>
-        );
-    }
 }
 export default AppointmentInformation;
