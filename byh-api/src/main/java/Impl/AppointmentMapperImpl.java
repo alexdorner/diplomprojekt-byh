a23da5164786a1c524cfbbe9621fa9ab678dbf8d
melaniepakchehr <melaniepakchehr@yahoo.com>
2021-03-11 16:24:06 +0100

    id appointments (participantid appointments (participant))

--- byh-api/src/main/java/Impl/AppointmentMapperImpl.java
+++ byh-api/src/main/java/Impl/AppointmentMapperImpl.java
@@ -23,32 +23,45 @@ public class AppointmentMapperImpl implements AppointmentMapper {
 
 
         Patient patient = new Patient();
+
         Reference ref2 = new Reference();
         ref2.setReference("Patient");
         ref2.setType("Patient");
-        ref2.setType(patientAppointmentK.getID());
         ref2.setId(patientAppointmentK.getID()+"_ref");
+        Participant pPatient = new Participant(ref2);
+        pPatient.setId("patient");
+        //participant.add(new Participant(ref2));
+        participant.add(pPatient);
 
-        participant.add(new Participant(ref2));
         appointment.setParticipant(participant);
         appointment.setId(patientAppointmentK.getName());
         appointment.setStart(patientAppointmentK.getAppointment_time());
+
         Reference ref3 = new Reference();
         ref3.setType(patientAppointmentK.getCompany());
         ref3.setId(patientAppointmentK.getCompany() + "_ref");
         ref3.setReference("http://localhost:8080/api/location/"+patientAppointmentK.getCompany());
-        participant.add(new Participant(ref3));
+        Participant pCompany = new Participant(ref3);
+        pCompany.setId("Location");
+        participant.add(pCompany);
+
         appointment.setCreated(patientAppointmentK.getAppointment_date());
+
         Reference ref4 = new Reference();
         ref4.setId(patientAppointmentK.getDepartment()+"_ref");
         ref4.setType(patientAppointmentK.getDepartment());
         ref4.setReference("http://localhost:8080/api/organization/" + patientAppointmentK.getDepartment());
-        participant.add(new Participant(ref4));
+        Participant pOrganization = new Participant(ref4);
+        pOrganization.setId("Organization");
+        participant.add(pOrganization);
+
         Reference ref5 = new Reference();
         ref5.setId(patientAppointmentK.getService_unit()+"_ref");
         ref5.setType(patientAppointmentK.getService_unit());
         ref5.setReference("http://localhost:8080/api/device/" +patientAppointmentK.getService_unit());
-        participant.add(new Participant(ref5));
+        Participant pDevice = new Participant(ref5);
+        pDevice.setId("Device");
+        participant.add(pDevice);
 
         return appointment;
     }
