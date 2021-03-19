6721b107d7deff8cbf90d1d6e01318c0554685da
jir17518 <jir17518@spengergasse.at>
2021-03-19 11:13:11 +0100

    update sortieren AppointmentOverview.js

--- bookyourhospital/src/Appointment/AppointmentOverview.js
+++ bookyourhospital/src/Appointment/AppointmentOverview.js
@@ -30,7 +30,7 @@ class AppointmentOverview extends Component {
     }
 
     async componentWillMount() {
-        const url = "http://localhost:8080/api/appointment?idOrganization="+this.department +"&idDevice="+this.serviceUnit;
+        const url = "192.189.51.8:8080/api/appointment?idOrganization="+this.department +"&idDevice="+this.serviceUnit;
         const response = await fetch(url).then(response => response.json()).then(recievedData => this.setState({data: recievedData}));
     }
 
